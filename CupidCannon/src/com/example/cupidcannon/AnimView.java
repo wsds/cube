ea1521d0f8a85ca83547f7089c86b6653e792962
bai <bai@163.com>
2012-08-01 13:08:32 +0800

    Add code to get the width and height of the screen

--- CupidCannon/src/com/example/cupidcannon/AnimView.java
+++ CupidCannon/src/com/example/cupidcannon/AnimView.java
@@ -8,40 +8,44 @@ import android.view.SurfaceView;
 
 public class AnimView extends SurfaceView implements SurfaceHolder.Callback,Runnable {
 	
-	 private SurfaceHolder mHolder;
-     private Thread mThread; 
-     public AnimView(Context context) {
-	      super(context);
-	      // TODO Auto-generated constructor stub
-	      mHolder = this.getHolder();
-	      mHolder.addCallback(this);
-	      mThread = new Thread(this);//创建一个绘图线程
-     }
+	private int mWidth;
+	private int mHeight;
+	private SurfaceHolder mHolder;
+    private Thread mThread; 
+    public AnimView(Context context) {
+	     super(context);
+	     // TODO Auto-generated constructor stub
+	     mHolder = this.getHolder();
+	     mHolder.addCallback(this);
+	     mThread = new Thread(this);//创建一个绘图线程
+    }
      
-     @Override  
-     public void surfaceChanged(SurfaceHolder holder, int format, int width,  
-             int height) {  
-         // TODO Auto-generated method stub  
-           
-     }  
-     @Override  
-     public void surfaceCreated(SurfaceHolder holder) {  
-       /*  mWidth=this.getWidth();  
+     
+    @Override  
+    public void surfaceCreated(SurfaceHolder holder) {  
+         mWidth=this.getWidth();  
          mHeight=this.getHeight();  
-         mThread.start(); */ 
-     }  
-     @Override  
-     public void surfaceDestroyed(SurfaceHolder holder) {  
-         // TODO Auto-generated method stub  
+         mThread.start();
+    }  
+    
+    @Override  
+    public void surfaceChanged(SurfaceHolder holder, int format, int width,  
+             int height) {  
+        // TODO Auto-generated method stub  
            
-     }  
-     
-     @Override  
-     public void run() {  
-         while(true){  
+    } 
+    @Override  
+    public void surfaceDestroyed(SurfaceHolder holder) {  
+        // TODO Auto-generated method stub  
+           
+    }  
+    
+    @Override  
+    public void run() {  
+    	while(true){  
   
-             try {  
-                 Thread.sleep(100);  
+            try {  
+                 	Thread.sleep(100);  
              } catch (InterruptedException e) {  
                  // TODO Auto-generated catch block  
                  e.printStackTrace();  
