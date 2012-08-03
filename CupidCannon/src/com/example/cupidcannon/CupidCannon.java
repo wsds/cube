13eb3633b16add385f02ca2e1446cb1293784978
bai <bai@163.com>
2012-08-03 11:25:01 +0800

    Put the class AnimView.java into CupidCannon.java. Modify the CanvasAnimation.java to fit the 2D animation. Add setElements() method to control bitmap and paint.

--- CupidCannon/src/com/example/cupidcannon/CupidCannon.java
+++ CupidCannon/src/com/example/cupidcannon/CupidCannon.java
@@ -1,8 +1,18 @@
 package com.example.cupidcannon;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Matrix;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
 
 public class CupidCannon extends Activity {
 
@@ -15,4 +25,88 @@ public class CupidCannon extends Activity {
         setContentView(new AnimView(this));
     }
     
+
+    public class AnimView extends SurfaceView implements SurfaceHolder.Callback,
+    		Runnable {
+
+    	private int mWidth = 0;
+    	private int mHeight = 0;
+    	private int mBitmapWidth = 0;
+    	private int mBitmapHeight = 0;
+    	private SurfaceHolder mHolder = null;
+    	private Thread mThread = null;
+    	private Bitmap mBitmap = null;
+    	private Canvas mCanvas = null;
+    	private Paint mPaint = null;
+    	private boolean isRunning=true;
+    	public AnimView(Context context) {
+    		super(context);
+    		// TODO Auto-generated constructor stub
+    		mHolder = this.getHolder();
+    		mHolder.addCallback(this);
+    		mThread = new Thread(this);// 创建一个绘图线程
+
+    	}
+
+    	private void initDraw() {
+
+    		mCanvas = mHolder.lockCanvas();
+
+    		mBitmap = BitmapFactory.decodeResource(getResources(),
+    				R.drawable.girl_4_2);
+    		mBitmapWidth = mBitmap.getWidth();
+    		mBitmapHeight = mBitmap.getHeight();
+
+    		mCanvas.drawRect(0, 0, mBitmap.getWidth(), mBitmap.getHeight(),
+    				new Paint());
+    		Matrix matrix = new Matrix();
+    		matrix.setScale(0.67f, 0.67f);
+    		matrix.postTranslate(0, -29);
+    		mCanvas.drawBitmap(mBitmap, matrix, mPaint);
+    		mHolder.unlockCanvasAndPost(mCanvas);
+    		if (mBitmap != null)
+    			mBitmap.recycle();
+
+    	}
+    	
+
+
+    	@Override
+    	public void surfaceCreated(SurfaceHolder holder) {
+    		mWidth = this.getWidth();
+    		mHeight = this.getHeight();
+    		initDraw();
+    		mThread.start();
+
+    	}
+
+    	@Override
+    	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+    			int height) {
+    		// TODO Auto-generated method stub
+
+    	}
+
+    	@Override
+    	public void surfaceDestroyed(SurfaceHolder holder) {
+    		// TODO Auto-generated method stub
+    		isRunning = false;
+
+    	}
+
+    	@Override
+    	public void run() {
+    		while (isRunning) {
+
+    			try {
+    				Thread.sleep(100);
+    			} catch (InterruptedException e) {
+    				// TODO Auto-generated catch block
+    				e.printStackTrace();
+    			}
+    		}
+    	}
+
+    }
+
 }
