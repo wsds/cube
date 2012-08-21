31dafcf080f523569b694076e7c0fefa74655094
wsds <wsds88801@163.com>
2012-08-21 12:57:19 +0800

    Mosquitoes can fly on the screen as self aware.

--- attract/src/com/cube/attract/game/mosquito/MosquitoActivity.java
+++ attract/src/com/cube/attract/game/mosquito/MosquitoActivity.java
@@ -20,6 +20,7 @@ import com.cube.attract.R;
 import com.cube.canvas.common.AnimationManager;
 import com.cube.canvas.common.AnimationManager.AnimationBitmap;
 import com.cube.canvas.common.CanvasAnimation2;
+import com.cube.canvas.common.CanvasAnimation2.Callback;
 
 public class MosquitoActivity extends Activity {
 
@@ -59,17 +60,25 @@ public class MosquitoActivity extends Activity {
 				public AnimationBitmap animationBitmap = null;
 				int x;
 				int y;
+				int type;
+				int directionX;
+				int directionY;
+				int direction;
+				int mWidth = 0;
+				int mHeight = 0;
 			}
 
 			public int count = 5;
 			public int type = 2;
 			public ArrayList<Mosquito> mosquitos = new ArrayList<Mosquito>();
+			Random random = null;
 
 			public void initaize() {
-				Random random = new Random(System.currentTimeMillis());
+				random = new Random(System.currentTimeMillis());
 				for (int i = 0; i < count; i++) {
 					Mosquito mosquito = new Mosquito();
 					int randomType = random.nextInt(1000) % type;
+					mosquito.type = randomType;
 					if (randomType == 0) {
 						mosquito.animationBitmap = animationManager.addAnimationBitmap(R.drawable.game2_mosquito1);
 					} else if (randomType >= 1) {
@@ -78,9 +87,42 @@ public class MosquitoActivity extends Activity {
 					Log.v(TAG, "randomType is " + randomType);
 					mosquito.x = random.nextInt(mWidth * 10) % mWidth;
 					mosquito.y = random.nextInt(600 * 10) % 600;
+					mosquito.direction = random.nextInt(1000) % 180;
+					// mosquito.direction = 45;
 					mosquito.animationBitmap.matrix.setTranslate(mosquito.x, mosquito.y);
+					mosquito.animationBitmap.matrix.preScale(0.25f, 0.25f);
 					mosquitos.add(mosquito);
+					fly(mosquito);
+
 				}
+			}
+
+			void fly(final Mosquito mosquito) {
+				// double factor = (random.nextInt(200) - 200) / 100;
+				// factor = 1;
+				mosquito.directionX = (int) (50 * Math.cos(mosquito.direction * Math.PI / 180));
+				mosquito.directionY = (int) (50 * Math.sin(mosquito.direction * Math.PI / 180));
+				Log.v(TAG, "direction is " + mosquito.direction + " and directionX is " + mosquito.directionX + " and directionY is " + mosquito.directionY);
+				CanvasAnimation2 flyAnimation = new CanvasAnimation2();
+				flyAnimation.setTranslate(mosquito.directionX, mosquito.directionY, 90);
+				flyAnimation.setCallback(new Callback() {
+					@Override
+					public void onEnd() {
+						mosquito.x = mosquito.x + mosquito.directionX;
+						mosquito.y = mosquito.y + mosquito.directionY;
+						if (mosquito.x < -50 || mosquito.x > mWidth + 50) {
+							mosquito.direction = (180 - mosquito.direction) % 360 + (random.nextInt(20) - 10);
+							// if ((0 < mosquito.direction && mosquito.direction < 90) || (0 >= mosquito.direction && mosquito.direction > -90)) {
+							mosquito.animationBitmap.matrix.preScale(-1, 1);
+							// }
+						}
+						if (mosquito.y < -50 || mosquito.y > mHeight * 0.8 - 150) {
+							mosquito.direction = -mosquito.direction + (random.nextInt(20) - 10);
+						}
+						fly(mosquito);
+					}
+				});
+				mosquito.animationBitmap.addAnimation(flyAnimation);
 			}
 		}
 
@@ -111,17 +153,16 @@ public class MosquitoActivity extends Activity {
 			shell.matrix.setTranslate(260, 550);
 
 			CanvasAnimation2 up = new CanvasAnimation2();
-			up.setTranslate(-100, -200,500);
+			up.setTranslate(-100, -200, 500);
 			// up.setRepeatSelfTimes(5);
 			up.setRepeatTimes(CanvasAnimation2.INFINITE);
-			
 
 			CanvasAnimation2 down = new CanvasAnimation2();
 			down.setTranslate(100, 200, 500);
 			down.setRepeatTimes(CanvasAnimation2.INFINITE);
 			up.addNextAnimation(down);
 			down.addNextAnimation(up);
-			
+
 			shell.addAnimation(up);
 
 			// CanvasAnimation2 turn = new CanvasAnimation2();
@@ -173,29 +214,38 @@ public class MosquitoActivity extends Activity {
 			float currentY = 0;
 			switch (action) {
 			case MotionEvent.ACTION_DOWN:
-				Log.v("test", "ACTION_DOWN");
 				startX = event.getX();
 				startY = event.getY();
+
+				currentX = startX;
+				currentY = startX;
 				break;
 			case MotionEvent.ACTION_MOVE:
-				Log.v("test", "ACTION_MOVE");
 				currentX = event.getX();
 				currentY = event.getY();
-				// moveLength = Math.sqrt((currentX - startX) * (currentX - startX) + (currentY - startY) * (currentY - startY));
+
 				break;
 			case MotionEvent.ACTION_UP:
-				Log.v("test", "ACTION_UP");
 				currentX = event.getX();
 				currentY = event.getY();
-				CanvasAnimation2 turn = new CanvasAnimation2();
-				turn.setRotate(1440, 23, 66, 1000);
-				turn.setRepeatSelfTimes(5);
-				shell.addAnimation(turn);
+				CanvasAnimation2 turn1 = new CanvasAnimation2();
+				turn1.setRotate(1440, 23, 66, 1000);
+				turn1.setRepeatSelfTimes(5);
+				shell.addAnimation(turn1);
 				// moveLength = Math.sqrt((currentX - startX) * (currentX - startX) + (currentY - startY) * (currentY - startY));
 				break;
 			}
-
+			turnCannon(currentX, currentY);
 			return true;
+		}
+
+		void turnCannon(float currentX, float currentY) {
+			double x = currentX - mWidth / 2;
+			double y = mHeight - 40 - currentY;
+			double theta = Math.atan2(x, y);
+			double angle = theta / Math.PI * 180;
+			cannon.matrix.setRotate((float) angle, 75, 106);
+			cannon.matrix.postTranslate((mWidth - 149) / 2, mHeight - 142);
 		}
 
 		class DrawThread implements Runnable {
