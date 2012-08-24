41d554dbfca578551969a690e31496ccba4c48d1
liurui <vitogump>
2012-08-24 14:43:07 +0800

    pickup function work,now i'm going to delete the ro.brite.android.opengl.common package

--- attract/src/com/cube/attract/entry/EntryActivity.java
+++ attract/src/com/cube/attract/entry/EntryActivity.java
@@ -1,19 +1,18 @@
-package com.cube.attract.entry;
+﻿package com.cube.attract.entry;
 
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.graphics.PixelFormat;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Window;
+import android.widget.Toast;
 
 public class EntryActivity extends Activity {
-//	private GLSurfaceView surface;
-//	private GlRenderer renderer;
 
-//	private GestureDetector gestureDetector;
 	private GLSurfaceView mGLSurfaceView;
-	SceneState sceneState = SceneState.getInstance();
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -26,19 +25,6 @@ public class EntryActivity extends Activity {
 		setContentView(mGLSurfaceView);
 		mGLSurfaceView.requestFocus();
 		mGLSurfaceView.setFocusableInTouchMode(true);
-////		gestureDetector = new GestureDetector(this, new GlAppGestureListener());
-//
-//		surface = new GLSurfaceView(this);
-//		renderer = new GlRenderer(this);
-//		surface.setRenderer(renderer);
-//		setContentView(surface);
-
-	}
-
-	@Override
-	protected void onPause() {
-		super.onPause();
-		mGLSurfaceView.onPause();
 	}
 
 	@Override
@@ -47,63 +33,16 @@ public class EntryActivity extends Activity {
 		mGLSurfaceView.onResume();
 	}
 
-//
-//	private float startX, startY;
-//
-//	@Override
-//	public boolean onTouchEvent(MotionEvent event) {
-//		if (gestureDetector.onTouchEvent(event)) {
-//			return true;
-//		}
-//
-//		switch (event.getAction()) {
-//		case MotionEvent.ACTION_DOWN:
-//			sceneState.isClicked = true;
-//			sceneState.dxSpeed = 0.0f;
-//			sceneState.dySpeed = 0.0f;
-//			startX = event.getX();
-//			startY = event.getY();
-//
-//			if (startY * 800 / sceneState.screenHeight < 150 && startY * 800 / sceneState.screenHeight > 80) {
-//				sceneState.eventType = sceneState.LOGO;
-////				renderer.logoDown.start(true);
-//			} else {
-//				sceneState.eventType = sceneState.CUB;
-//				sceneState.saveRotation();
-//			}
-//
-//			break;
-//		case MotionEvent.ACTION_MOVE:
-//
-//			if (sceneState.eventType == sceneState.CUB) {
-//				sceneState.dx = event.getX() - startX;
-//				sceneState.dy = event.getY() - startY;
-//			}
-//			break;
-//			
-//		case MotionEvent.ACTION_UP:
-//			sceneState.gbNeedPick = true;
-////			if (sceneState.eventType == sceneState.LOGO) {
-////				renderer.logoUp.start(true);
-////			} 
-//			break;
-//		}
-//
-//		return super.onTouchEvent(event);
-//	}
-//
-//
-//	private class GlAppGestureListener extends GestureDetector.SimpleOnGestureListener {
-//		@Override
-//		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-//			if (sceneState.eventType == sceneState.LOGO) {
-//			} else if (sceneState.eventType == sceneState.CUB) {
-//				sceneState.dxSpeed = velocityX / 1000;
-//				sceneState.dySpeed = velocityY / 1000;
-//			}
-//
-//			return super.onFling(e1, e2, velocityX, velocityY);
-//		}
-//	}
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mGLSurfaceView.onPause();
+	}
 
+	private Handler myHandler = new Handler() {
+		@Override
+		public void handleMessage(Message msg) {
+			Toast.makeText(EntryActivity.this, "选中了" + msg.what + "面", Toast.LENGTH_SHORT).show();
+		}
+	};
 }
